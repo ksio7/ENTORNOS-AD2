5343624e9899f13fe900331b6d86d2bce9119c3b
ksio7 <sergiusparedes@gmail.com>
2023-02-20 17:40:44 +0100

    añadidos los tests

---
+++ src/JugadorTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JugadorTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testPonerDorsal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstaExpulsado() {
+		fail("Not yet implemented");
+	}
+
+}
